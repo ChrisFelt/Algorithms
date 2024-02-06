df60f27341cf225643cc9f0efeb7621c3b3d2942
ChrisFelt <christopher.f.felt@gmail.com>
2024-02-06 16:25:31 -0700

    palindrome number fun

---
+++ palindrome_number/palindrome_number.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # handle edge cases
+        if x < 0:
+            return False
+
+        # single digit numbers are a palindrome
+        if x < 10:
+            return True
+
+        # mod and divide by 10 until each digit in x is separated into a list (O(n) complexity)
+        # deque data structure gives O(1) complexity for append/pop on both ends of the stack
+        x_deque = deque([])
+        while x >= 10:
+            x_deque.appendleft(x % 10)
+            x = x // 10  # remove last digit
+        x_deque.appendleft(x)
+
+        # compare the digits and check if palindrome
+        # Time complexity for recursion through the deque: O(n)
+        return self.recursePalindrome(x_deque)
+
+    def recursePalindrome(self, deque):
+        """
+        :type deque: Deque[int]
+        :rtype: bool
+        """
+        cur_len = len(deque)
+        # base case 1: 1 or 0 digits remaining
+        if cur_len == 1 or cur_len == 0:
+            return True
+
+        # base case 2: left and right side do not match
+        elif deque.popleft() != deque.pop():
+            return False
+
+        else:
+            return self.recursePalindrome(deque)
---
+++ palindrome_number/tests.py
@@ -0,0 +1,67 @@
+import unittest
+from palindrome_number import Solution
+
+
+class TestCheckPwd(unittest.TestCase):
+
+    # test the given solutions
+    def test_001(self):
+        # setup test
+        x = 121
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is True)
+
+    def test_002(self):
+        # setup test
+        x = -121
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is False)
+
+    def test_003(self):
+        # setup test
+        x = 10
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is False)
+
+    # additional test cases
+    def test_004(self):
+        # setup test
+        x = 0
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is True)
+
+    def test_005(self):
+        # setup test
+        x = 1111111111111
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is True)
+
+    def test_006(self):
+        # setup test
+        x = 232323232323
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is False)
+
+    def test_007(self):
+        # setup test
+        x = 23233232
+        check_solution = Solution()
+
+        # call isPalindrome method from Solution to test
+        self.assertTrue(check_solution.isPalindrome(x) is True)
+
+
+if __name__ == '__main__':
+    unittest.main()
