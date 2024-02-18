ad8d285555dac4aec3fbd56e50166b84dcd57549
ChrisFelt <christopher.f.felt@gmail.com>
2024-02-17 21:54:39 -0700

    fix tests

--- path_sum/tests.py
+++ path_sum/tests.py
@@ -7,30 +7,30 @@ class TestSolution(unittest.TestCase):
     # test the given solutions
     def test_001(self):
         # setup test
-        tree = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1]
-        target = 22
         check_solution = Solution()
-        solution = check_solution.has_path_sum(tree, target)
+        root = check_solution.gen_tree([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1])
+        target = 22
+        solution = check_solution.has_path_sum(root, target)
 
         # compare answer to solution
         self.assertTrue(solution)
 
     def test_002(self):
         # setup test
-        tree = [1, 2, 3]
-        target = 5
         check_solution = Solution()
-        solution = check_solution.has_path_sum(tree, target)
+        root = check_solution.gen_tree([1, 2, 3])
+        target = 5
+        solution = check_solution.has_path_sum(root, target)
 
         # compare answer to solution
         self.assertFalse(solution)
 
     def test_003(self):
         # setup test
-        tree = []
-        target = 0
         check_solution = Solution()
-        solution = check_solution.has_path_sum(tree, target)
+        root = check_solution.gen_tree([])
+        target = 0
+        solution = check_solution.has_path_sum(root, target)
 
         # compare answer to solution
         self.assertFalse(solution)
