c540a85521004796cf22e424bd210a541df5fd44
ChrisFelt <christopher.f.felt@gmail.com>
2024-02-17 21:45:47 -0700

    fix TreeNode class

--- path_sum/path_sum.py
+++ path_sum/path_sum.py
@@ -1,9 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self._val = val
+        self._left = left
+        self._right = right
+
+    def set_val(self, val):
+        self._val = val
+
+    def set_left(self, node):
+        self._left = node
+
+    def set_right(self, node):
+        self._right = node
+
+    def get_val(self):
+        return self._val
+
+    def get_left(self):
+        return self._left
+
+    def get_right(self):
+        return self._right
+
 
 class Solution(object):
     def has_path_sum(self, root, target_sum):
