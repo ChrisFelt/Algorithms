203a45197b35fd84162b3135ef7e1dfaae537c01
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-13 00:36:08 -0700

    working solution

--- excel_sheet_col_no/title_to_no.py
+++ excel_sheet_col_no/title_to_no.py
@@ -1,3 +1,14 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        pass
+        # time complexity of this solution: O(n), where n is the length of the input string
+        sum = 0
+        pos = 0
+
+        # sum the following:
+        # 26 ** position from end * char_val
+        for char in columnTitle[::-1]:
+            sum += 26 ** pos * (ord(char) - 64)
+            pos += 1
+
+        return sum
+    
