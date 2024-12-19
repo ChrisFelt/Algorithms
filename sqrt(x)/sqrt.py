4a2275e06e473e8237c5c63208e928c62de2061c
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-19 00:23:13 -0700

    working solution

--- sqrt(x)/sqrt.py
+++ sqrt(x)/sqrt.py
@@ -1,4 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        pass
-    
+        """Using Newton's method"""
+        approx = x
+
+        # iterative approximations
+        while approx * approx > x:
+            # Newton's method applied to finding square roots: x(n+ 1) = 1/2 * (n + a/n), where a is the radicand
+            approx = (approx + x // approx) // 2  
+        
+        return approx
--- sqrt(x)/tests.py
+++ sqrt(x)/tests.py
@@ -24,6 +24,37 @@ class TestSolution(unittest.TestCase):
 
         # compare answer to solution
         self.assertTrue(test_solution == output)
+    
+    # other tests
+    def test_003(self):
+        # setup test
+        check_solution = Solution()
+        x = 3
+        output = 1
+        test_solution = check_solution.mySqrt(x)
+
+        # compare answer to solution
+        self.assertTrue(test_solution == output)
+    
+    def test_004(self):
+        # setup test
+        check_solution = Solution()
+        x = 0
+        output = 0
+        test_solution = check_solution.mySqrt(x)
+
+        # compare answer to solution
+        self.assertTrue(test_solution == output)
+    
+    def test_005(self):
+        # setup test
+        check_solution = Solution()
+        x = 9
+        output = 3
+        test_solution = check_solution.mySqrt(x)
+
+        # compare answer to solution
+        self.assertTrue(test_solution == output)
 
 
 if __name__ == '__main__':
