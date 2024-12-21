90ea034263212670cdfd223720b8d3119ed8d14f
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-21 01:04:43 -0700

    initial setup

---
+++ climbing_stairs/climb_stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        pass
+        # recursion to branch off at each point 1 or 2 could be added
+        # def stairs_rec(n: int) -> int:
+        # base case: n <= 0 return 0
+        # base case: n > 0 return 1
+        # call stairs_rec with (n - 2) + (n - 1)
---
+++ climbing_stairs/tests.py
@@ -0,0 +1,30 @@
+import unittest
+from climb_stairs import Solution
+
+
+class TestSolution(unittest.TestCase):
+
+    # test the given solutions
+    def test_001(self):
+        # setup test
+        check_solution = Solution()
+        n = 2
+        output = 2
+        test_solution = check_solution.climbStairs(n)
+
+        # compare answer to solution
+        self.assertTrue(test_solution == output)
+
+    def test_002(self):
+        # setup test
+        check_solution = Solution()
+        n = 3
+        output = 3
+        test_solution = check_solution.climbStairs(n)
+
+        # compare answer to solution
+        self.assertTrue(test_solution == output)
+
+
+if __name__ == '__main__':
+    unittest.main()
