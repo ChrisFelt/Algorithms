15934b1a5ca6df99c55fb1c316a90093166a22d4
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-21 01:21:47 -0700

    brute force solution: too slow

--- climbing_stairs/climb_stairs.py
+++ climbing_stairs/climb_stairs.py
@@ -1,8 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        pass
-        # recursion to branch off at each point 1 or 2 could be added
-        # def stairs_rec(n: int) -> int:
-        # base case: n <= 0 return 0
-        # base case: n > 0 return 1
-        # call stairs_rec with (n - 2) + (n - 1)
+        # Time complexity of this solution: appears to be O(n!) ...
+
+        # recursively explore all paths
+        def stairs_rec(s: int) -> int:
+            # base case 1: bad path
+            if s < 0:
+                return 0
+            # base case 2: good path
+            if s == 0:
+                return 1
+            
+            # check both 1 or 2 steps from current path
+            return stairs_rec(s - 2) + stairs_rec(s - 1)
+
+        return stairs_rec(n)
