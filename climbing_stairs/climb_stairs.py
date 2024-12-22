baed7eac2f8b5616379c093c50bf14004b5e6437
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-22 02:11:10 -0700

    dynamic programming solution

--- climbing_stairs/climb_stairs.py
+++ climbing_stairs/climb_stairs.py
@@ -1,9 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Time complexity of this solution: appears to be O(n!) ...
-        # Todo: try memoizing - should have same pattern as fibonacci sequence
+        # Time complexity of this solution: O(n)
+        # store solution at each value of n computed
+        # n[0] is skipped in stairs_rec because n >= 1
+        memo = [-1] * (n + 1)
 
-        # recursively explore all paths
+        # recursively explore all paths from the top down
         def stairs_rec(s: int) -> int:
             # base case 1: bad path
             if s < 0:
@@ -11,8 +13,13 @@ class Solution:
             # base case 2: good path
             if s == 0:
                 return 1
+            # base case 3: already calculated possible steps at this value of s
+            if memo[s] != -1:
+                return memo[s]
             
-            # check both 1 or 2 steps from current path
-            return stairs_rec(s - 2) + stairs_rec(s - 1)
+            # check both 1 or 2 steps from current path and store value in memo
+            memo[s] = stairs_rec(s - 2) + stairs_rec(s - 1)
+
+            return memo[s]  # final element of memo is the solution
 
         return stairs_rec(n)
