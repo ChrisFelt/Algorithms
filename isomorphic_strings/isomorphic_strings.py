07594b740ae45ae2a6b2cab77813f264936d13d8
ChrisFelt <christopher.f.felt@gmail.com>
2024-12-23 21:29:21 -0700

    working solution

--- isomorphic_strings/isomorphic_strings.py
+++ isomorphic_strings/isomorphic_strings.py
@@ -1,33 +1,24 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # time complexity for this solution: 2n + n = O(n)
-        # construct dictionaries for each word with number of times each letter appears
-        s_count = self.count_char(s)
-        t_count = self.count_char(t)
-
-        # compare counts in place 
-        # (converting to lists and comparing the lists would be O(n^2) since list append has a time complexity of O(n) in Python)
+        # Time complexity of this solution: O(n)
+        # use dictionary to track whether the same chars were at the same indices or not
+        s_dict = {}
+        t_dict = {}
+        
+        # iterate over both strings simultaneously
         for i in range(len(s)):
-            # if count of char count for the string at i is not the same for s and t, they are not isomorphs
-            if s_count[s[i]] != t_count[t[i]]:
+            # add character to the dictionary if it does not yet exist
+            # hash map lookup time complexity in Python: average case O(1), worst case O(n)
+            if s[i] not in s_dict:
+                s_dict[s[i]] = i
+            if t[i] not in t_dict:
+                t_dict[t[i]] = i
+            
+            # characters should always appear at the same index across strings
+            # if character already exists in the dictionary but has a different index, then the strings are not isomorphs
+            if s_dict[s[i]] != t_dict[t[i]]:
                 return False
         
         return True
         
-
-    def count_char(self, a_string: str) -> dict:
-        """Counts each character in a string and returns result as a dictionary"""
-
-        # construct dictionary to count number of character occurences
-        string_dict = {}
-        for i in a_string:
-            # add 1 to count if char exists
-            if i in string_dict:
-                string_dict[i] += 1
-            
-            # add new entry if char not in dictionary
-            else:
-                string_dict[i] = 1
-        
-        return string_dict
         
